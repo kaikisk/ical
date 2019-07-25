6452267013ecc69bdef0145f11ed4ecb304fccbb
kaikis <kueobei@gmail.com>
2019-07-25 11:26:54 +0900

    commit

--- createIcs.js
+++ createIcs.js
@@ -2,12 +2,14 @@ function createIcs(){
     var date = $("#txtDate").val();
     var detail = $("#description").val();
 
+    var dat1 = date.replace(/-/g, '')
+
     var ics = "BEGIN:VCALENDAR\n"
     +"VERSION:1.0\n"
     +"BEGIN:VEVENT\n"
     +"CATEGORIES:MEETING\n"
     +"STATUS:TENTATIVE\n"
-    +"DTSTART:"+ date + "T000000Z\n"
+    +"DTSTART:"+ date1 + "T000000Z\n"
     +"SUMMARY:がんの検診日\n"
     +"DESCRIPTION:" + detail + "\n"
     +"CLASS:PRIVATE\n"
