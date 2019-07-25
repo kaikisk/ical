9441865df78cbb7e1b15f08942f450ea029f069c
kaikis <kueobei@gmail.com>
2019-07-25 11:53:21 +0900

    commit

--- createIcs.js
+++ createIcs.js
@@ -1,10 +1,12 @@
+var ics = '';
+
 function createIcs(){
     var date = $("#txtDate").val();
     var detail = $("#description").val();
 
     var date1 = date.replace(/-/g, '');
 
-    var ics = "BEGIN:VCALENDAR\n"
+    ics = "BEGIN:VCALENDAR\n"
     +"VERSION:1.0\n"
     +"BEGIN:VEVENT\n"
     +"CATEGORIES:MEETING\n"
@@ -17,4 +19,22 @@ function createIcs(){
     +"END:VCALENDAR\n"
 
     console.log(ics);
+}
+
+function download() {
+    var file = new Blob([ics], {type: ics});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file,"calendar");
+    else { // Others
+        var a = $("#downloadIcs"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = calendar;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
 }
